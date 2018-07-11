60a4da393ab55ce5194ca5002c81af16ab4dff31
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-07-11 10:27:22 +0200

    hshshshs

--- src/components/games/GameDetails.jsx
+++ src/components/games/GameDetails.jsx
@@ -1,83 +1,80 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-import {getGames, joinGame, updateGame} from '../../actions/games'
-import {getUsers} from '../../actions/users'
-import {userId} from '../../jwt'
-import Paper from 'material-ui/Paper'
-import Board from './Board'
-import './GameDetails.css'
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { getGames, joinGame, updateGame } from "../../actions/games";
+import { getUsers } from "../../actions/users";
+import { userId } from "../../jwt";
+import Paper from "material-ui/Paper";
+import Board from "./Board";
+import "./GameDetails.css";
 
 class GameDetails extends PureComponent {
-
   componentWillMount() {
     if (this.props.authenticated) {
-      if (this.props.game === null) this.props.getGames()
-      if (this.props.users === null) this.props.getUsers()
+      if (this.props.game === null) this.props.getGames(); //als er nog geen game is geladen dan haalt hij een game op
+      if (this.props.users === null) this.props.getUsers(); // als de users nog niet zijn ingeladen haalt hij de users op
     }
   }
 
-  joinGame = () => this.props.joinGame(this.props.game.id)
+  joinGame = () => this.props.joinGame(this.props.game.id); //checks whitch users are connected to one game and gives them permission to play
 
   makeMove = (toRow, toCell) => {
-    const {game, updateGame} = this.props
+    //parameters index of row and index of column
+    const { game, updateGame } = this.props; //short notation for this.props.game and this.props.updateGame
 
     const board = game.board.map(
-      (row, rowIndex) => row.map((cell, cellIndex) => {
-        if (rowIndex === toRow && cellIndex === toCell) return game.turn
-        else return cell
-      })
-    )
-    updateGame(game.id, board)
-  }
-
-
+      //game.board is het board hoe het staat in de database.. alle rows worden gemapt. Rows zijn ook weer arrays
+      (row, rowIndex) =>
+        row.map((cell, cellIndex) => {
+          //rowindex is hetzelfde als column//de columns worden hier gemapt
+          if (rowIndex === toRow && cellIndex === toCell) {
+            console.log("celindex ", cellIndex, " rowIndex ", rowIndex);
+            return game.turn;
+          } //
+          else return cell;
+        })
+    );
+    updateGame(game.id, board);
+  };
 
   render() {
-    const {game, users, authenticated, userId} = this.props
+    const { game, users, authenticated, userId } = this.props;
 
-    if (!authenticated) return (
-			<Redirect to="/login" />
-		)
+    if (!authenticated) return <Redirect to="/login" />;
 
-    if (game === null || users === null) return 'Loading...'
-    if (!game) return 'Not found'
+    if (game === null || users === null) return "Loading...";
+    if (!game) return "Not found";
 
-    const player = game.players.find(p => p.userId === userId)
+    const player = game.players.find(p => p.userId === userId);
 
     const winner = game.players
       .filter(p => p.symbol === game.winner)
-      .map(p => p.userId)[0]
+      .map(p => p.userId)[0];
 
-    return (<Paper className="outer-paper">
-      <h1>Game #{game.id}</h1>
+    return (
+      <Paper className="outer-paper">
+        <h1>Game #{game.id}</h1>
 
-      <p>Status: {game.status}</p>
+        <p>Status: {game.status}</p>
 
-      {
-        game.status === 'started' &&
-        player && player.symbol === game.turn &&
-        <div>It's your turn!</div>
-      }
+        {game.status === "started" &&
+          player &&
+          player.symbol === game.turn && <div>It's your turn! </div>}
 
-      {
-        game.status === 'pending' &&
-        game.players.map(p => p.userId).indexOf(userId) === -1 &&
-        <button onClick={this.joinGame}>Join Game</button>
-      }
+        {game.status === "pending" &&
+          game.players.map(p => p.userId).indexOf(userId) === -1 && (
+            <button onClick={this.joinGame}>Join Game</button>
+          )}
 
-      {
-        winner &&
-        <p>Winner: {users[winner].firstName}</p>
-      }
+        {winner && <p>Winner: {users[winner].firstName}</p>}
 
-      <hr />
+        <hr />
 
-      {
-        game.status !== 'pending' &&
-        <Board board={game.board} makeMove={this.makeMove} />
-      }
-    </Paper>)
+        {game.status !== "pending" && (
+          <Board board={game.board} makeMove={this.makeMove} />
+        )}
+      </Paper>
+    );
   }
 }
 
@@ -86,10 +83,16 @@ const mapStateToProps = (state, props) => ({
   userId: state.currentUser && userId(state.currentUser.jwt),
   game: state.games && state.games[props.match.params.id],
   users: state.users
-})
+});
 
 const mapDispatchToProps = {
-  getGames, getUsers, joinGame, updateGame
-}
+  getGames,
+  getUsers,
+  joinGame,
+  updateGame
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(GameDetails)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GameDetails);
